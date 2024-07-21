6b457bd7f838e279bdba352f36fbd8ae5d78d8a7
erindhoxha <hello@erindhoxha.dev>
2024-07-21 13:26:07 +0200

    Change home page design a bit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Button, H1, H2, XStack } from 'tamagui';
 export default function Tab() {
   return (
     <View style={styles.container}>
-      <H1 marginBottom={12}>Ku</H1>
+      <H1 marginBottom={12}>Ku?</H1>
       <Text>Platforme te cilin e boni share me shoke dhe shoqe, dhe votoni ku eshte destinacioni juaj i ardhshem.</Text>
       <Text
         style={{
@@ -13,18 +13,29 @@ export default function Tab() {
         }}>
         Zgjedhni nje nga opsionet me poshte:
       </Text>
-      <XStack gap="$3">
-        <Link href="../sessionForm" asChild>
-          <Button theme="green" marginTop={12}>
-            Krijo nje session
-          </Button>
-        </Link>
-      </XStack>
-      <H2 marginTop={24}>Sessions</H2>
-      <Text>Keni 3 sessions pa match</Text>
+
+      <H2 marginTop={24}>Menagjo sesionet</H2>
+      <Text
+        style={{
+          marginTop: 8,
+        }}>
+        Menagjoni sessionet, dhe mund ti nderroni detajet
+      </Text>
       <Link href="./sessions" asChild>
         <Button theme="blue" marginTop={12}>
           Shiko sessionet
+        </Button>
+      </Link>
+      <H2 marginTop={24}>Krijo nje sesion</H2>
+      <Text
+        style={{
+          marginTop: 8,
+        }}>
+        Krijo nje session, dhe pastaj shperndaje me grupin:
+      </Text>
+      <Link href="../sessionForm" asChild>
+        <Button theme="green" marginTop={12}>
+          Krijo nje session
         </Button>
       </Link>
     </View>
